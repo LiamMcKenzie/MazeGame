0fe7a2ec9958ccbaa3449762fbfdf9d553a7b06e
Liam Mckenzie <mckeld3@student.op.ac.nz>
2023-04-05 10:45:09 +1200

    added 3d movement

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     private MazeConstructor constructor;
+    public GameObject playerPrefab;
+    public GameObject monsterPrefab;
 
     [SerializeField] private int rows;
     [SerializeField] private int cols;
@@ -18,5 +20,22 @@ public class GameController : MonoBehaviour
     void Start()
     {
         constructor.GenerateNewMaze(rows, cols);
+
+        CreatePlayer();
+        CreateMonster();
+    }
+
+    private void CreatePlayer()
+    {
+        Vector3 playerStartPosition = new Vector3(constructor.hallWidth, 1, constructor.hallWidth);  
+        GameObject player = Instantiate(playerPrefab, playerStartPosition, Quaternion.identity);
+        player.tag = "Generated";
+    }
+
+    private void CreateMonster()
+    {
+        Vector3 monsterPosition = new Vector3(constructor.goalCol * constructor.hallWidth, 0f, constructor.goalRow * constructor.hallWidth);
+        GameObject monster = Instantiate(monsterPrefab, monsterPosition, Quaternion.identity);
+        monster.tag = "Generated";    
     }
 }
